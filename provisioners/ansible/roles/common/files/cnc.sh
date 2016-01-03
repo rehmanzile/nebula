c4f878f878e641945628ae6f574adb72dca8beb2
zile <zile@cloudgenix.com>
2016-01-03 11:12:55 -0800

    updates.

--- provisioners/ansible/roles/common/files/cnc.sh
+++ provisioners/ansible/roles/common/files/cnc.sh
@@ -34,28 +34,3 @@ lpass show --field "Public Key" ssh-key > ~/.ssh/id_rsa.pub
 PRIVATE_KEY=~/.ssh/id_rsa
 lpass show --field "Private Key" ssh-key > ${PRIVATE_KEY}
 lpass logout --force
-
-## Workaround for LastPassword Bug: https://github.com/lastpass/lastpass-cli/issues/135
-#sed -i 's/-----BEGIN RSA PRIVATE KEY-----/BEGIN/g' ${PRIVATE_KEY}
-#sed -i 's/-----END RSA PRIVATE KEY-----/END/g' ${PRIVATE_KEY}
-#sed -i 's/ /\n/g' ${PRIVATE_KEY}
-#sed -i 's/BEGIN/-----BEGIN RSA PRIVATE KEY-----/g' ${PRIVATE_KEY}
-#sed -i 's/END/-----END RSA PRIVATE KEY-----/g' ${PRIVATE_KEY}
-
-
-echo "Setting up development environment"
-echo -e "Host stash.internal.cloudgenix.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-MY_PROJECT=~/myproject
-if [ ! -d "${MY_PROJECT}/lineups" ]; then
-    pushd ${MY_PROJECT}
-    git clone -b feature/AUTO-1046 ssh://git@stash.internal.cloudgenix.com:7999/maserati/lineups.git
-    lineups/bin/lineup init
-    popd
-fi
-
-echo "Adding environment variables to ~/.bashrc file..."
-grep -q -F 'export CGNX_ACCOUNT' ~/.bashrc || echo "export CGNX_ACCOUNT=${CGNX_ACCOUNT}" >> ~/.bashrc
-grep -q -F '~/myproject/lineups/bin' ~/.bashrc || echo 'PATH=$PATH:~/myproject/lineups/bin' >> ~/.bashrc
-echo ""
-echo "source ~/.bashrc     <--- Use this command to apply changes to your environment"
-echo ""
